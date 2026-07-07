3d997c6a8aa29c3f51616e96cb9e104d5f605633
NAITIK-AGARWAL939 <agarwalnaitik2007@gmail.com>
2026-07-07 09:17:27 -0700

    error removing
    Removed comment about showing real errors during development.

--- 25ESKCX067/DAY-9/config.php
+++ 25ESKCX067/DAY-9/config.php
@@ -7,7 +7,7 @@ $dbpass = "12345";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $dbuser, $dbpass);
-    // Show real errors during development
+    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
