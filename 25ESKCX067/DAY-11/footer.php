98546f86bd50708482bb9017d945eb3964280f88
NAITIK-AGARWAL939 <agarwalnaitik2007@gmail.com>
2026-07-10 10:51:38 -0700

    Add files via upload

---
+++ 25ESKCX067/DAY-11/footer.php
@@ -0,0 +1,12 @@
+<footer class ="bg-dark text-white text-centre py-3 mt-5">
+    <center>
+        <p class ="mb-0">&copy;<?php echo date("Y");?>
+            My Website.All Rights Reserved.</p>
+    </center>
+</footer>
+
+
+<!--bootstrap js-->
+<script src ="bootstrap.bundle.min.js"></script>
+</body>
+</html>
